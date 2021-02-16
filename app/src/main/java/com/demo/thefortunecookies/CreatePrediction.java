eb0e710ca7db049fa8a830592b7e2d4907147b8f
avasilistov <kvoroncova@gmail.com>
2021-02-16 03:08:31 +0200

    Finish.

--- app/src/main/java/com/demo/thefortunecookies/CreatePrediction.java
+++ app/src/main/java/com/demo/thefortunecookies/CreatePrediction.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreatePrediction extends AppCompatActivity {
     private EditText editTextName;
@@ -22,9 +23,45 @@ public class CreatePrediction extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editTextName = findViewById(R.id.editTextTextPersonName);
         editTextAge = findViewById(R.id.editTextAge);
+        Log.i("TAG", "onCreate");
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("TAG", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("TAG", "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("TAG", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("TAG", "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("TAG", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("TAG", "onDestroy");
+    }
 
     public void getPrediction(View view) {
         if (editTextName.length() != 0 && editTextAge.length() != 0) {
@@ -38,7 +75,7 @@ public class CreatePrediction extends AppCompatActivity {
             intent.putExtra("name", name);
             startActivity(intent);
         } else {
-
+            Toast.makeText(this, "Fill all fields please", Toast.LENGTH_SHORT).show();
         }
     }
 
