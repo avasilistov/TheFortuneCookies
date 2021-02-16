c0f5a40457785825afd7e08abf9300be89293245
avasilistov <kvoroncova@gmail.com>
2021-02-16 03:11:09 +0200

    Finish.

--- app/src/main/java/com/demo/thefortunecookies/DescriptionOfPrediction.java
+++ app/src/main/java/com/demo/thefortunecookies/DescriptionOfPrediction.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DescriptionOfPrediction extends AppCompatActivity {
@@ -16,6 +17,41 @@ public class DescriptionOfPrediction extends AppCompatActivity {
         Intent intent = getIntent();
         prediction = findViewById(R.id.textViewPrediction);
         prediction.setText(intent.getStringExtra("name") + " " +    intent.getStringExtra("prediction"));
+        Log.i("TAG", "onCreate");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("TAG", "onStart");
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("TAG", "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("TAG", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("TAG", "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("TAG", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("TAG", "onDestroy");
     }
 }
